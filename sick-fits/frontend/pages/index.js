3bea3eca516e0234274c5b17a0a7d7a6f04a26f8
Vinayak Phal <vphal@paypal.com>
2020-12-04 14:18:31 +0530

    Completed module-2

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const Home = props => (
+  <div>
+    <h2>Home Page!</h2>
+
+  </div>
+)
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Sell = props => (
+  <div>
+    <h2>Sell Page!</h2>
+    <Link href="/">
+      <a>Home!</a>
+    </Link>
+  </div>
+)
+
+export default Sell;
